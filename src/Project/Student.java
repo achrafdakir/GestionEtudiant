4adad6c576061e2e749e0d9a8b95ae0292b0b14a
Achraf Dakir <achraf01dakir@gmail.com>
2022-11-20 02:51:24 +0100

    interface graphique

---
+++ src/Project/Student.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+
+/**
+ *
+ * @author achraf
+ */
+public class Student {
+    private String nom,prenom;
+    private int age;
+    private static int inscription;
+
+    public Student(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+        inscription++;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static int getInscription() {
+        return inscription;
+    }
+
+    
+}
